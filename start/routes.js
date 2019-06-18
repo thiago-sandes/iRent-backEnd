c6beec996f818dedeb2107ae90d846e0f01dd9b4
thiago-sandes <thiago-sandes@hotmail.com>
2019-06-18 15:27:44 -0300

    Base back-end do projeto com autenticação.

--- start/routes.js
+++ start/routes.js
@@ -3,3 +3,6 @@
 const Route = use('Route')
 
 Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
+
+Route.get('/app', 'AppController.index').middleware(['auth'])
