32839880148fbbc8c4afc933c8651361fd34d858
kelveng.magalhaes <kelveng.magalhaes@tupiconsultoria.com.br>
2019-06-19 22:50:37 -0300

    criando seed do token

---
+++ database/seeds/TokenSeeder.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| TokenSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class TokenSeeder {
+  async run () {
+  }
+}
+
+module.exports = TokenSeeder
