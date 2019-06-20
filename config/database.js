084abd3a11a1fc1a5f7894ce30dd9dc7848cac35
kelveng.magalhaes <kelveng.magalhaes@tupiconsultoria.com.br>
2019-06-20 13:43:58 -0300

    ajustando database.js para o heroku

--- config/database.js
+++ config/database.js
@@ -3,6 +3,9 @@
 /** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
 
+const Url = require('url-parse')
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
@@ -16,7 +19,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get('DB_CONNECTION', 'pg'),
 
   /*
   |--------------------------------------------------------------------------
@@ -69,7 +72,7 @@ module.exports = {
   |
   | npm i --save pg
   |
-  */
+  
   pg: {
     client: 'pg',
     connection: {
@@ -79,6 +82,16 @@ module.exports = {
       password: Env.get('DB_PASSWORD', ''),
       database: Env.get('DB_DATABASE', 'adonis')
     },
+    */
+    pg: {
+      client: 'pg',
+      connection: {
+         host: Env.get('DB_HOST', DATABASE_URL.hostname),
+         port: Env.get('DB_PORT', DATABASE_URL.port),
+         user: Env.get('DB_USER', DATABASE_URL.username),
+         password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+         database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+       }
+    },
     debug: Env.get('DB_DEBUG', false)
   }
-}
