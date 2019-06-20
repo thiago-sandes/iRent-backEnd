f8b92aecc3b584c3b9634bf709a5f846415bb558
kelveng.magalhaes <kelveng.magalhaes@tupiconsultoria.com.br>
2019-06-19 22:42:48 -0300

    alterando nome do model para user

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -5,7 +5,7 @@ const Schema = use('Schema')
 
 class UserSchema extends Schema {
   up () {
-    this.create('users', (table) => {
+    this.create('user', (table) => {
       table.increments()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
@@ -15,7 +15,7 @@ class UserSchema extends Schema {
   }
 
   down () {
-    this.drop('users')
+    this.drop('user')
   }
 }
 
