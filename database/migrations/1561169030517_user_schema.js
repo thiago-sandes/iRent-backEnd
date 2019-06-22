03f4770d57677008a089fb3f8c3e93c365174fe7
kelveng.magalhaes <kelveng.magalhaes@tupiconsultoria.com.br>
2019-06-21 23:09:35 -0300

    corrigindo id

--- database/migrations/1561169030517_user_schema.js
+++ database/migrations/1561169030517_user_schema.js
@@ -7,14 +7,10 @@ class UserSchema extends Schema {
   up () {
     this.table('users', (table) => {
       // alter table
-      table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
       table.string('telephone', 30).notNullable()
       table.string('name', 254).notNullable()
       table.string('sex', 1).notNullable()
-      table.timestamps()
+    
     })
   }
 
