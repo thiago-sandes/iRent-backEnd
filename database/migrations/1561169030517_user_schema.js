f1ca094dd5dd01822fa99b2caddea55fcfea78a4
kelveng.magalhaes <kelveng.magalhaes@tupiconsultoria.com.br>
2019-06-21 23:05:09 -0300

    corrigindo alter table

---
+++ database/migrations/1561169030517_user_schema.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class UserSchema extends Schema {
+  up () {
+    this.table('users', (table) => {
+      // alter table
+      table.increments()
+      table.string('username', 80).notNullable().unique()
+      table.string('email', 254).notNullable().unique()
+      table.string('password', 60).notNullable()
+      table.string('telephone', 30).notNullable()
+      table.string('name', 254).notNullable()
+      table.string('sex', 1).notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.table('users', (table) => {
+      // reverse alternations
+    })
+  }
+}
+
+module.exports = UserSchema
